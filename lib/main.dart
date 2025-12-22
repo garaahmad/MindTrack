f4f601091faf35772f4d670da214499faf1acc03
Ahmad Algarably <140180784+garaahmad@users.noreply.github.com>
2025-12-22 15:19:54 +0200

    remove API

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'onboarding_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Firebase disabled for local testing
-  Gemini.init(apiKey: 'AIzaSyDdmXKgnTM1XYmTadSAs4lDYzvN9sBgg8Y');
+  Gemini.init(apiKey: 'APY--- Don/t use my API');
   runApp(const MyApp());
 }
 
